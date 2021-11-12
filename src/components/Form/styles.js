e19d0903e228c674a76bafe34b96e2bbbd06c6fa
Filipe Melo <filipexpert63@gmail.com>
2021-11-12 18:46:47 -0300

    Ajustes: bug na section do Form resolvida.

--- src/components/Form/styles.js
+++ src/components/Form/styles.js
@@ -4,16 +4,11 @@ export const SectionStyled = styled.section`
   display: flex;
   justify-content: center;
   flex-direction: column;
-  width: 70%;
-  background: rgb(1 4 13 50%);
   box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
-  backdrop-filter: blur(2.5px);
-  -webkit-backdrop-filter: blur(2.5px);
-  border-radius: 10px;
   border: 1px solid rgba(255, 255, 255, 0.18);
   margin: 0 auto;
   padding: 1.5rem;
-  height: 50vh;
+  min-height: 50vh;
   width: 65vw;
   border-radius: 6px;
 
